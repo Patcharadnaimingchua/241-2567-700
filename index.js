245be8919d2d799d1014eda199f00682ed2feb83
Patcharadnai <189591998+Patcharadnaimingchua@users.noreply.github.com>
2025-02-10 13:06:52 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,78 @@
-function submitData (){
-    let firstNameDOM = document.querySelector('input[name="firstname"]'); 
-    let lastNameDOM = document.querySelector('input[name="lastname"]');
-    let ageDOM = document.querySelector('input[name="age"]');
-    let genderDOM = document.querySelector('input[name="gender"]:checked');
-    let interestDOMs = document.querySelectorAll('input[name="interest"]:checked');
-    let descriptionDOM = document.querySelector('textarea[name="description"]');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-    let interest ='';
-    for(let i = 0; i < interestDOMs.length; i++){
-        interest += interestDOMs[i].value
-        if(i != interestDOMs.length - 1){
-            interest += ','
+const port = 8000;
+app.use(bodyParser.json());
+
+let users = []
+let counter = 1
+
+/*
+GET /users สำหรับ get users ทั้งหมดที่บันทึกไว้
+POST /users สำหรับสร้าง users ใหม่บันทึกเข้าไป
+GET /users/:id สำหรับดึง users รายคนออกมา
+PUT /users/:id สำหรับแก้ไข users รายคน (ตาม id ที่บันทึกเข้าไป)
+DELETE /users/:id สำหรับลบ users รายคน (ตาม id ที่บันทึกเข้าไป)
+*/
+
+
+
+// path: = GET /users สำหรับ get users ทั้งหมดที่บันทึกไว้
+app.get('/users', (req, res) => {
+    res.json(users);
+})
+
+// path: = POST /users สำหรับสร้าง users ใหม่บันทึกเข้าไป
+app.post('/user', (req,res) => {
+    let user = req.body;
+    user.id = counter
+    counter += 1
+    users.push(user);
+    res.json({
+        message: ' Create new user successfully',
+        user: user
+    });
+}) 
+
+//path: PUT /user/:id ใช้สำหรับแก้ไขข้อมูล user โดยใช้ id เป็น
+app.put('/user/:id',(req,res) => {
+    let id = req.params.id;
+    let updatedUser = req.body;
+    //หา  users จาก id ที่ส่งมา
+    let selectedIndex = users.findIndex(user => user.id == id) 
+    
+    //แก้ๆขข้อมูล user
+    if (updatedUser.firstname) {
+        user[selectedIndex].firstname = updatedUser.firstname
+    }
+    if (updatedUser.lastname) {
+        users[selectedIndex].lastname = updatedUser.lastname
+    }
+    res.json({
+        message: 'Update user successfully',
+        data: {
+            user: updatedUser,
+            indexUpdate: selectedIndex
         }
-    }
+    })
+})
 
 
-    let userData ={
-        firstName: firstNameDOM.value,
-        lastName: lastNameDOM.value,
-        age: ageDOM.value,
-        gender: genderDOM.value,
-        description: descriptionDOM.value,
-        interest: interest
-    }
-    console.log('submitData',userData);
-}
+//path: DELETE /user/:id ใช้สำหรับลบข้อมูล user โดยใช่ id เป็นตัวระบุ
+app.delete('/user/:id', (req, res) => {
+    let id = req.params.id;
+    //หา index ของ user ที่ต้องการลบ
+    let selectedIndex = users.findIndex(user => user.id == id)
+
+    //ลบ
+    users.splice(selectedIndex, 1)
+    res.json({
+        message: 'Delete user successfully',
+        indexedDeleted: selectedIndex
+    })
+})
+
+app.listen(port, (req,res) => {
+    console.log('Http Server is running on port' + port)
+});
